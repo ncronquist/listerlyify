260910cbf92c479ea43775ef81cd8a23827b9a37
ncronquist <nick@ncronquist.com>
2015-04-14 12:53:57 -0700

    Add async calls to get list members for each list

--- controllers/list.js
+++ controllers/list.js
@@ -1,7 +1,8 @@
 // Requires
 var express = require('express'),
     ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
-    Twitter = require('twitter');
+    Twitter = require('twitter'),
+    async = require('async');
 
 // Variables
 var router = express.Router();
@@ -21,16 +22,45 @@ router.get('/listeditor', ensureLoggedIn('/'), function(req,res) {
     if (!error) {
       listgridinfo.lists = lists;
 
-      var friends_params = {count: 200, skip_status: true, include_user_entities: false};
-      client.get('friends/list', friends_params, function(error, friends, response) {
-        if(!error) {
-          listgridinfo.friends = friends;
-          // res.send(listgridinfo);
-          res.render('list/listeditor', listgridinfo);
-        } else {
-          res.send('there was an error');
-        }
+      var listArray = [];
+      lists.lists.forEach(function(list, idx, array) {
+        listArray.push(list.id);
       })
+
+      listgridinfo.listArray = listArray;
+
+      var listmembers = function (list_id, doneCallback) {
+        // Call back with no error and the result of num * num
+        var members_params = {list_id: list_id, count: 5000, include_entities: false};
+        client.get('lists/members', members_params, function(error, members, response) {
+          if (!error) {
+            return doneCallback(null, {list_id: list_id, members: members});
+          } else {
+            res.send('There was an error trying to get list members');
+          }
+        })
+        // return doneCallback(null, num * num);
+      };
+
+      // Square each number in the array [1, 2, 3, 4]
+      async.map(listArray, listmembers, function (err, results) {
+        // Square has been called on each of the numbers
+        // so we're now done!
+        console.log("Finished!");
+        console.log(results);
+        listgridinfo.listmembers = results;
+
+        var friends_params = {count: 200, skip_status: true, include_user_entities: false};
+        client.get('friends/list', friends_params, function(error, friends, response) {
+          if(!error) {
+            listgridinfo.friends = friends;
+            res.send(listgridinfo);
+            // res.render('list/listeditor', listgridinfo);
+          } else {
+            res.send('there was an error');
+          }
+        })
+      });
     } else {
       res.send('there was an error');
     }
